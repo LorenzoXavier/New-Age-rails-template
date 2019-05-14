87c8632bbeb28803bfdade602f4633a7687687cb
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2019-05-14 17:49:49 +0700

    complete

---
+++ app/assets/javascripts/new_age/manifest.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery.easing
+//= require turbolinks
+//= require bootstrap
+//= require new_age/new_age
---
+++ app/assets/javascripts/new_age/new_age.js
@@ -0,0 +1,57 @@
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("navbar-shrink");
+    } else {
+        $(".navbar-fixed-top").removeClass("navbar-shrink");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 100
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 50
+        }
+    })
+
+})(jQuery); // End of use strict
+
